0d004f073b1303aea983fae57f245902d53f0b4b
yatusabebeibe <jesus.temgal@educa.jcyl.es>
2026-01-11 19:02:39 +0100

    style: eslint formatting

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ import { createApp } from "./app.js";
 const PORT = process.env.PORT ?? 3000;
 
 const app = createApp();
-const server = createServer(app);
+const server = createServer( app );
 
-server.listen(3000, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+server.listen( 3000, () => {
+  console.log( `Server is running on http://localhost:${PORT}` );
 });
