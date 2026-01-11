a2cd413022289eb2a0af36a55735236592b789fe
yatusabebeibe <jesus.temgal@educa.jcyl.es>
2026-01-11 20:42:47 +0100

    chore: add configurations to express app

--- app.js
+++ app.js
@@ -4,7 +4,11 @@ import { router } from "./routes/index.routes.js";
 export const createApp = () => {
   const app = express();
 
+  app.disable( "x-powered-by" );
+
   app.use( express.json() );
+  app.set( "view engine", "ejs" );
+  app.set( "views", "./views" );
   app.use( router() );
 
   return app;
