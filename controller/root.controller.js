f1f4a377363f3058b56cb95d8de04b418263b345
yatusabebeibe <jesus.temgal@educa.jcyl.es>
2026-01-11 20:50:52 +0100

    feat: add and render basic EJS template at "/"

--- controller/root.controller.js
+++ controller/root.controller.js
@@ -2,7 +2,7 @@ export class RootController {
   x = 10;
 
   getRoot = ( req, res ) => {
-    res.json({"message":"Hello, World!", "x": this.x});
+    res.render( "index", { x: this.x });
     this.x += 1;
   };
 }
