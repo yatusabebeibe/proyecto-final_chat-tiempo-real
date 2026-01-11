0737ad39853bfa52042ca46b83835a15c1e8d1ba
yatusabebeibe <jesus.temgal@educa.jcyl.es>
2026-01-11 19:06:56 +0100

    feat: add router to handle requests more specifically

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 import express from "express";
+import { router } from "./routes/index.routes";
 
 export const createApp = () => {
   const app = express();
+
+  app.use( express.json() );
+  app.use( router() );
+
   return app;
 };
---
+++ routes/index.routes.js
@@ -0,0 +1,8 @@
+import { Router } from "express";
+
+export const router = () => {
+  const router = Router();
+
+
+  return router;
+};
