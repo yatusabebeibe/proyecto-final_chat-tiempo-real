0b6acc8e17632052f14dc2848834de2a23be46d6
yatusabebeibe <jesus.temgal@educa.jcyl.es>
2026-01-13 10:03:02 +0100

    feat: add static file serving from public folder

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ export const createApp = () => {
   app.use( express.json() );
   app.set( "view engine", "ejs" );
   app.set( "views", "./views" );
+  app.use( "/", express.static( "public" ) );
   app.use( router() );
 
   return app;
